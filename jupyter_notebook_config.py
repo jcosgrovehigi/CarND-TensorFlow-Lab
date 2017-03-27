0918feb70f6c928e75b1c5691611e99895d7d9b6
jcosgrovehigi <jcosgrove@higi.com>
2017-03-27 11:12:32 -0500

    Disabled security

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -20,6 +20,8 @@ c.NotebookApp.ip = '*'
 c.NotebookApp.port = int(os.getenv('PORT', 8888))
 c.NotebookApp.open_browser = False
 c.MultiKernelManager.default_kernel_name = 'python'
+c.NotebookApp.token = ''
+c.NotebookApp.password = ''
 
 # sets a password if PASSWORD is set in the environment
 if 'PASSWORD' in os.environ:
